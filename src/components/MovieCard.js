3da348931e5c0bfd41a1546a179fb77f0d09694e
nick.waddington <Nick.Waddington@zonedigital.com>
2020-03-17 12:28:36 +0000

    latest

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -7,19 +7,6 @@ class MovieCard extends React.Component {
 
             return (
 							<li className="movie-card">
-								
-								{/* <img
-									className="movie-card__image"
-									src="https://via.placeholder.com/500"
-									alt={this.props.title}
-                                />
-                                <h2 className="movie-card__title">Title</h2>
-								<ul className="movie-card__genres">
-									<li className="movie-card__genre">Lorem</li>
-									<li className="movie-card__genre">Ipsum</li>
-									<li className="movie-card__genre">Dolor</li>
-								</ul> */}
-
 								<img
 									className="movie-card__image"
 									src={"https://image.tmdb.org/t/p/w500" + this.props.image}
